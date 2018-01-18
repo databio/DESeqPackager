9ff06bf640242b61c4b6488b63c5659e04ed91de
Aaron Gu <ag5ym@virginia.edu>
2018-01-18 17:31:34 -0500

    changed function parameters

--- DESeq-Packager.R
+++ DESeq-Packager.R
@@ -1,71 +1,67 @@
-install.packages("data.table")
-library("data.table")
 source("https://bioconductor.org/biocLite.R")
 biocLite("DESeq")
 library("DESeq")
 
-#importing pepr project
-devtools::install_github("pepkit/pepr")
-library(pepr)
-p <- Project(file = "project_config.yaml")
-sample_frame <- samples(p)
-files <- sample_frame[ , data_source]
-
-#prompting user to rename files 
-parameters <- c()
-while(TRUE){
-  s <- readline(prompt= "Rename each file with sample identifiers (Type DONE when finished): ")
-  if(s=="DONE"){
-    rm(s)
-    break
+#' packages multiple data tables into one data frame for DESeq analysis
+#'
+#' @param yaml file path to project's yaml
+#' @return countTable, the data structure needed to input into DESeq
+#' @export
+DESeq_table_maker <- function(yaml, data_source){
+  
+  #importing pepr project
+  devtools::install_github("pepkit/pepr")
+  library(pepr)
+  p <- Project(file = yaml)
+  sample_frame <- samples(p)
+  files <- sample_frame[ , data_source]
+  type <- sample_frame[ , sample_name]
+  
+  #prompting user to specify columns for DESeq Analysis
+  gene_name_col <- readline(prompt= "Specify which column in each data table is the gene name column: ")
+  relevant_data_col <- readline(prompt= "Specify which column contains the relevant data for DESeq analysis: ")
+  
+  print("Packaging...")
+  
+  #reading in the files
+  df_list <- vector(mode="list", length=length(files))
+  for(i in 1:length(files)){
+    sampleTable <- read.table(files[i], header = TRUE)
+    sampleTable <- sampleTable[, c(gene_name_col, relevant_data_col)]
+    sampleTable[[2]] <- as.integer(sampleTable[[2]]) #integers are required for DESeq
+    names(sampleTable)[2] <- type[i]
+    df_list[[i]] <- sampleTable
   }
-  parameters <- c(parameters, s)
-}
-type <- c()
-for(column in parameters){
-  type <- paste(type, sample_frame[, get(column)], sep = "")
-}
-rm(parameters, column)
+  
+  #function to merge the datatables in the list
+  countTable <- merge(df_list[[1]], df_list[[2]], by = gene_name_col)
+  for(i in 3:length(df_list)){
+    countTable <- merge(countTable, df_list[[i]])
+  }
 
-#prompting user to specify columns for DESeq Analysis
-gene_name_col <- readline(prompt= "Specify which column in each data table is the gene name column: ")
-relevant_data_col <- readline(prompt= "Specify which column is the relevant data for DESeq analysis: ")
-
-#reading in the files
-df_list <- vector(mode="list", length=length(files))
-for(i in 1:length(files)){
-  sampleTable <- read.table(files[i], header = TRUE)
-  sampleTable <- sampleTable[, c(gene_name_col, relevant_data_col)]
-  sampleTable[[2]] <- as.integer(sampleTable[[2]]) #integers are required for DESeq
-  names(sampleTable)[2] <- paste(type[i], relevant_data_col, sep = "_")
-  df_list[[i]] <- sampleTable
+  #set the row names as the gene names, then remove the gene name column
+  row.names(countTable) <- countTable[[1]]
+  countTable[,1] <- NULL
+  
+  print("Finished! View the new data frame in the variable called 'countTable'")
+  return(countTable)
 }
 
-#function to merge the datatables in the list
-countTable <- merge(df_list[[1]], df_list[[2]], by=gene_name_col)
-for(i in 3:length(df_list)){
-  countTable <- merge(countTable, df_list[[i]])
-}
-#set the row names as the gene names, then remove the gene name column
-row.names(countTable) <- countTable[[1]]
-countTable[,1] <- NULL
+countTable <- DESeq_table_maker("Documents/GitHub/DESeq-Packager/project_config.yaml", "data_source")
 
+
+
+#-----------------DESeq Analysis-------------------------#
 #set the conditions......this could be passed in as a parameter to an enclosing function
-condition <- factor(c("knockout", "knockout", "control", "control"))
-condition2 <- factor(c("treatment", "treatment", "control", "control"))
-
-
-
+condition <- factor(c("controlDMSO","controlDMSO","knockoutDMSO","knockoutDMSO"))
 #run DESeq analysis
 cds <- newCountDataSet(countTable, condition)
-cds2 <- newCountDataSet(countTable2, condition2)
-cds2 <- estimateSizeFactors(cds2)
+cds <- estimateSizeFactors(cds)
 
 cds <- estimateDispersions(cds, fitType = "local")
 plotDispEsts(cds)
 
-res <- nbinomTest(cds, "knockout", "control")
-head(res)
+res <- nbinomTest(cds, "controlDMSO", "knockoutDMSO")
 plotMA(res)
 
 hist(res$pval, breaks =100, col='skyblue', border = 'slateblue', main = '')
